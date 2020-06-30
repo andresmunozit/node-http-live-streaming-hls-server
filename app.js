f1891593bf1ea65e8788265b547a21327be8da36
Andres Munoz <am@andresmunozit.com>
2020-06-30 02:34:15 -0500

    Generate the playlist m3u8 automatically

--- app.js
+++ app.js
@@ -1,40 +1,20 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const playListGenerator = require('./helpers/playlistGenerator');
 
 const PORT = 3000;
 const VIDEO_EXT = 'ts';
 const VIDEO_FILE_PATH = path.join(__dirname, `video/beach.${VIDEO_EXT}`);
-const CHUNK_SIZE = 10000000;
-const PLAYLIST = `\
-#EXTM3U
-#EXT-X-MEDIA-SEQUENCE:0
-#EXT-X-TARGETDURATION:2
+const SEGMENT_SIZE = 10000000;
 
-#EXTINF:2,
-0.${VIDEO_EXT}
-#EXTINF:2,
-1.${VIDEO_EXT}
-#EXTINF:2,
-2.${VIDEO_EXT}
-#EXTINF:2,
-3.${VIDEO_EXT}
-#EXTINF:2,
-4.${VIDEO_EXT}
-#EXTINF:2,
-5.${VIDEO_EXT}
-#EXTINF:2,
-6.${VIDEO_EXT}
-#EXTINF:2,
-7.${VIDEO_EXT}
-#EXTINF:2,
-8.${VIDEO_EXT}
-`;
+const size = fs.statSync(VIDEO_FILE_PATH).size;
+const playlist = playListGenerator(size, SEGMENT_SIZE, 'ts');
 
-const videoChunkStream = (videoFilePath, sequence, chunkSize) => {
+const videoSegmentStream = (videoFilePath, sequence, segmentSize) => {
     if(!fs.existsSync(videoFilePath)) return {error: 'File not found'};
-    const start = sequence * chunkSize;
-    const end = start + chunkSize;
+    const start = sequence * segmentSize;
+    const end = start + segmentSize;
 
     return fs.createReadStream(videoFilePath, {start, end});
 };
@@ -48,7 +28,7 @@ const server = http.createServer( (req, res) => {
             'Content-Disposition':'attachment; playlist.m3u8'
         };
         res.writeHead(200, headers);
-        res.write(PLAYLIST);
+        res.write(playlist);
         res.end()
 
     } else if (req.url.match(`.${VIDEO_EXT}`)){
@@ -57,22 +37,21 @@ const server = http.createServer( (req, res) => {
 
         const size = fs.statSync(VIDEO_FILE_PATH).size;
         const headers = {
-            // 'Content-Length': `${CHUNK_SIZE}`,
+            // 'Content-Length': `${SEGMENT_SIZE}`,
             // 'Content-Type': 'video/mp2t'
             'Content-Type': 'video/mp2t'
         };
         
+        const sequence = req.url.replace(`.${VIDEO_EXT}`, '').replace('/seq-','');
+        const videoSegmentStreamToSend = videoSegmentStream(VIDEO_FILE_PATH, +sequence, SEGMENT_SIZE);
 
-        const sequence = req.url.replace(`.${VIDEO_EXT}`, '').replace('/','');
-        const videoChunkStreamToSend = videoChunkStream(VIDEO_FILE_PATH, +sequence, CHUNK_SIZE);
-
-        if(videoChunkStreamToSend.error) {
+        if(videoSegmentStreamToSend.error) {
             res.writeHead(500);
             return res.end();
         };
 
         res.writeHead(200, headers);
-        videoChunkStreamToSend.pipe(res, {end: true});
+        videoSegmentStreamToSend.pipe(res, {end: true});
     } else {
         res.writeHead(404); // Default will always be 200
     };
