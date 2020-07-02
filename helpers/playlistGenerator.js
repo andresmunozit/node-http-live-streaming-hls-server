06a24a89b3f64bb2977a58a8ca5db94e6545982b
Andres Munoz <am@andresmunozit.com>
2020-07-02 00:40:55 -0500

    Add the media name and stream name to the playlist generator

--- helpers/playlistGenerator.js
+++ helpers/playlistGenerator.js
@@ -9,22 +9,17 @@ const playListStart = `\
 const playListEnd = `
 #EXT-X-ENDLIST`;
 
-const segmentTemplate = (sequence, fileExtension, segmentDuration, stream) => {
-    return `#EXTINF:${segmentDuration},\n${stream ? `/${stream}` : ''}seq-${sequence}.${fileExtension}\n`;
+const segmentTemplate = (sequence, fileExtension, segmentDuration, mediaName, stream) => {
+    return `#EXTINF:${segmentDuration},\n${mediaName}/${stream ? `${stream}/` : ''}seq-${sequence}.${fileExtension}\n`;
 };
 
-const playListGenerator = (videoFileSize, segmentSize, fileExtension, videoDurationInSeconds, stream) => {
-
+const playListGenerator = (videoFileSize, segmentSize, fileExtension, videoDurationInSeconds, mediaName, stream) => {
     const segmentDuration = (videoDurationInSeconds * (+segmentSize / +videoFileSize)).toFixed(1);
-
     const segmentsCount = parseInt(videoFileSize / segmentSize);
-
     let playListBody = '';
-    
     for (let sequence = 0; sequence < segmentsCount; sequence++) {
-        playListBody = playListBody + segmentTemplate(sequence, fileExtension, segmentDuration);
+        playListBody = playListBody + segmentTemplate(sequence, fileExtension, segmentDuration, mediaName, stream);
     };
-
     return playListStart + playListBody + playListEnd;
 };
 
